d7756afdfa66859432b8fe8db69ea0a0cfdd7764
MethoD112 <mileshko.vitaly@gmail.com>
2017-01-29 13:00:18 +0300

    Fully complited

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,46 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        if (this.max < this.min) {
+            return;
+        }
+        if (this.max === this.min) {
+            return this.max;
+        } else if (this.max - this.min === 1) {
+            return this.min;
+        } else {
 
+            this.value = Math.ceil((this.max + this.min) / 2);
+            //console.log('Guess ' + this.value);
+        }
+
+        return this.value;
     }
 
     lower() {
-
+        this.setRange(this.min, this.value);
+        //console.log('lower ' + this.min, this.max);
     }
 
     greater() {
-
+        this.setRange(this.value, this.max);
+        //console.log('greater ' + this.min, this.max);
     }
+
+    write() {
+    	console.log('value: ' + this.value + ' range(' + this.min + ' ' + this.max + ')');
+    }
+
+
 }
 
 module.exports = GuessingGame;
